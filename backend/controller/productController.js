16aa8b7ad64eb69b847f2f8669da3c9997a1b698
Kainat Bibi <kainatlatifkhan09@gmail.com>
2026-02-05 06:21:06 -0800

    Add search, filter and pagination functionality to products API

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,52 +1,78 @@
 import Product from '../models/productModel.js'
 import handleError from "../utils/handleError.js";
 import handleAsyncError from '../middleware/handleAsyncError.js';
+import APIFunctionality from '../utils/apiFunctionality.js';
+
+// http://localhost:8000/api/v1/product/697f0c2db163600f6d8b1938?keyword=shirt 
 
 // Creating Products 
 
 export const createProducts = handleAsyncError(async (req, res, next) => {
-        const product = await Product.create(req.body)
-        res.status(201).json({ success: true, product })
-    })
+    const product = await Product.create(req.body)
+    res.status(201).json({ success: true, product })
+})
 
 // Get All Products 
 
-export const getAllProducts = handleAsyncError( async (req, res, next) => {
-    const products = await Product.find()
-    res.status(200).json({ success: true, products })
+export const getAllProducts = handleAsyncError(async (req, res, next) => {
+    const resultPerPage = 3;
+    const apiFeatures = new APIFunctionality(Product.find(), req.query).search().filter();
+
+    // Getting filtered query before pegination 
+
+    const filteredQuery = apiFeatures.query.clone();
+    const productCount = await filteredQuery.countDocuments();
+
+    //    Calculate total pages based on filtered count 
+
+    const totalPages = Math.ceil(productCount / resultPerPage);
+    const page = Number(req.query.page) || 1;
+    if(page>totalPages && productCount>0){
+        return next(new handleError("This page doesn't exist" , 404))
+    }
+
+    // Apply peination 
+
+    apiFeatures.pagination(resultPerPage);
+   // const products = await Product.find()
+    const products = await apiFeatures.query;
+    if(!products || products.length===0){
+        return next(new handleError("no product found", 404))
+    }
+    res.status(200).json({ success: true, products, productCount, totalPages, resultPerPage, currentPage:page})
 })
 
 // Update Product
 
-export const updateProduct = handleAsyncError (async (req, res,next) => {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!product) {
-            return next(new handleError("Product not found" , 404))
-        }
-        res.status(200).json({ success: true, product })
+export const updateProduct = handleAsyncError(async (req, res, next) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
     })
+    if (!product) {
+        return next(new handleError("Product not found", 404))
+    }
+    res.status(200).json({ success: true, product })
+})
 
 // Delete Product 
 
-export const deleteProduct = handleAsyncError(async (req, res,next) => {
+export const deleteProduct = handleAsyncError(async (req, res, next) => {
     const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) {
-        return next(new handleError("Product not found" , 404))
+        return next(new handleError("Product not found", 404))
     }
-    res.status(200).json({ success: true, message:"Product deleted successfully!" })
+    res.status(200).json({ success: true, message: "Product deleted successfully!" })
 })
 
 // Accessing Single Product 
 
-export const getSingleProduct = handleAsyncError (async(req,res,next)=>{
+export const getSingleProduct = handleAsyncError(async (req, res, next) => {
     const product = await Product.findById(req.params.id)
-    if(!product){
-        return next(new handleError("Product not found" , 404))
+    if (!product) {
+        return next(new handleError("Product not found", 404))
     }
-    res.status(200).json({success:true,product})
+    res.status(200).json({ success: true, product })
 
 })
 
